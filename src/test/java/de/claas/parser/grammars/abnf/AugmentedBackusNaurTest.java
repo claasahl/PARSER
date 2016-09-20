4bb82df460c273efe78aaf5f8eba6c6d854f37c8
Claas Ahlrichs <claasahl@gmail.com>
2016-09-20 16:41:28 +0200

    preliminary implementation of "node-tree-generator" for rules

--- src/test/java/de/claas/parser/grammars/abnf/AugmentedBackusNaurTest.java
+++ src/test/java/de/claas/parser/grammars/abnf/AugmentedBackusNaurTest.java
@@ -3,12 +3,23 @@ package de.claas.parser.grammars.abnf;
 import static org.junit.Assert.assertNotNull;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import org.junit.Test;
 
 import de.claas.parser.Grammar;
 import de.claas.parser.GrammarTest;
+import de.claas.parser.Node;
+import de.claas.parser.Rule;
+import de.claas.parser.results.NonTerminalNode;
+import de.claas.parser.results.TerminalNode;
+import de.claas.parser.rules.Conjunction;
+import de.claas.parser.rules.Disjunction;
+import de.claas.parser.rules.NonTerminal;
+import de.claas.parser.rules.Optional;
+import de.claas.parser.rules.Repetition;
+import de.claas.parser.rules.Terminal;
 
 /**
  * 
@@ -29,7 +40,10 @@ public class AugmentedBackusNaurTest extends GrammarTest<AugmentedBackusNaur> {
 	@Test
 	public void shouldHandleConcatenation() {
 		Grammar grammar = build();
-		assertNotNull(grammar.parse("rule = \"hel\" \"lo\"\r\n", false));
+		Node actual = grammar.parse("rule = \"hel\" \"lo\"\r\n", false);
+
+		Node expected = generateNodes(new NonTerminal("rule", new Conjunction(new Terminal("hel"), new Terminal("lo"))));
+		assertEquals(expected, actual);
 	}
 
 	@Test
@@ -122,6 +136,178 @@ public class AugmentedBackusNaurTest extends GrammarTest<AugmentedBackusNaur> {
 		Grammar grammar = build();
 		String data = String.join("\r\n", rules) + "\r\n";
 		assertNotNull(grammar.parse(data, false));
+	}
+
+	private static Node generateNodes(NonTerminal actualRule) {
+		Node rulename = new NonTerminalNode("rulename");
+		String name = actualRule.getName();
+		for (int index = 0; index < name.length(); index++) {
+			Node alpha = new NonTerminalNode("alpha");
+			String terminal = name.substring(index, index+1);
+			alpha.addChild(new TerminalNode(terminal));
+			rulename.addChild(alpha);
+		}
+
+		Node definedAs = new NonTerminalNode("defined-as");
+		Node wsp = new NonTerminalNode("wsp");
+		wsp.addChild(new TerminalNode(" "));
+		Node cwsp = new NonTerminalNode("c-wsp");
+		cwsp.addChild(wsp);
+		definedAs.addChild(cwsp);
+		definedAs.addChild(new TerminalNode("="));
+		definedAs.addChild(cwsp);
+
+		Node elements = new NonTerminalNode("elements");
+		elements.addChild(generateAlternation(actualRule.getRule()));
+
+		Node CRLF = new NonTerminalNode("crlf");
+		CRLF.addChild(new TerminalNode("\r\n"));
+		Node NL = new NonTerminalNode("c-nl");
+		NL.addChild(CRLF);
+
+		Node rule = new NonTerminalNode("rule");
+		rule.addChild(rulename);
+		rule.addChild(definedAs);
+		rule.addChild(elements);
+		rule.addChild(NL);
+
+		Node rulelist = new NonTerminalNode("rulelist");
+		rulelist.addChild(rule);
+		return rulelist;
+	}
+
+	private static Node generateAlternation(Rule actualRule) {
+		Node alternation = new NonTerminalNode("alternation");
+		if (actualRule.getClass().isAssignableFrom(Disjunction.class)) {
+			boolean firstChild = true;
+			for (Rule child : actualRule) {
+				if (!firstChild)
+					alternation.addChild(new TerminalNode("/"));
+				Node concatenation = generateConcatenation(child);
+				alternation.addChild(concatenation);
+				firstChild = false;
+			}
+		} else if (actualRule.getClass().isAssignableFrom(Terminal.class)) {
+			Terminal rule = (Terminal) actualRule;
+			Iterator<String> terminals = rule.getTerminals();
+			boolean firstTerminal = true;
+			
+			while(terminals.hasNext()) {
+				if (!firstTerminal)
+					alternation.addChild(new TerminalNode("/"));
+				
+				String terminal = terminals.next();
+				Node concatenation = generateConcatenation(new Terminal(terminal));
+				alternation.addChild(concatenation);
+				firstTerminal = false;				
+			}
+		} else {
+			alternation.addChild(generateConcatenation(actualRule));
+		}
+		return alternation;
+	}
+
+	private static Node generateConcatenation(Rule actualRule) {
+		Node concatenation = new NonTerminalNode("concatenation");
+		if (actualRule.getClass().isAssignableFrom(Conjunction.class)) {
+			boolean firstChild = true;
+			for (Rule child : actualRule) {
+				if (!firstChild) {
+					Node wsp = new NonTerminalNode("wsp");
+					wsp.addChild(new TerminalNode(" "));
+					Node cwsp = new NonTerminalNode("c-wsp");
+					cwsp.addChild(wsp);
+					concatenation.addChild(cwsp);
+				}
+				
+				Node repetition = generateRepetition(child);
+				concatenation.addChild(repetition);
+				firstChild = false;			
+			}
+		} else {
+			concatenation.addChild(generateRepetition(actualRule));
+		}
+		return concatenation;
+	}
+
+	private static Node generateRepetition(Rule actualRule) {
+		Node repetition = new NonTerminalNode("repetition");
+		if (actualRule.getClass().isAssignableFrom(Repetition.class)) {
+			Repetition rule = (Repetition) actualRule;
+
+			Node repeat = new NonTerminalNode("repeat");
+			int min = rule.getMinimumNumberOfRepetions();
+			int max = rule.getMaximumNumberOfRepetions();
+			if (min == max) {
+				repeat.addChild(new TerminalNode(Integer.toString(min)));
+			} else {
+				if (min != 0)
+					repeat.addChild(new TerminalNode(Integer.toString(min)));
+				repeat.addChild(new TerminalNode("*"));
+				if (max != Integer.MAX_VALUE)
+					repeat.addChild(new TerminalNode(Integer.toString(max)));
+			}
+			repetition.addChild(repeat);
+			repetition.addChild(generateElement(rule.getRule()));
+		} else {
+			repetition.addChild(generateElement(actualRule));
+		}
+		return repetition;
+	}
+
+	private static Node generateElement(Rule actualRule) {
+		Node element = new NonTerminalNode("element");
+		if (actualRule.getClass().isAssignableFrom(NonTerminal.class)) {
+			NonTerminal rule = (NonTerminal) actualRule;
+			Node rulename = new NonTerminalNode("rulename");
+			String name = rule.getName();
+			for (int index = 0; index < name.length(); index++) {
+				Node alpha = new NonTerminalNode("alpha");
+				String terminal = name.substring(index, index);
+				alpha.addChild(new TerminalNode(terminal));
+				rulename.addChild(alpha);
+			}
+			element.addChild(rulename);
+		} else if (actualRule.getClass().isAssignableFrom(Terminal.class)) {
+			Terminal rule = (Terminal) actualRule;
+			Iterator<String> terminals = rule.getTerminals();
+			String terminal = terminals.next();
+
+			Node charVal = new NonTerminalNode("char-val");
+			Node caseInsensitiveAtring = new NonTerminalNode("case-insensitive-string");
+			charVal.addChild(caseInsensitiveAtring);
+			Node quotedString = new NonTerminalNode("quoted-string");
+			caseInsensitiveAtring.addChild(quotedString);
+			
+			Node dQuote = new NonTerminalNode("dQuote");
+			dQuote.addChild(new TerminalNode("\""));
+			quotedString.addChild(dQuote);
+			for (int index = 0; index < terminal.length(); index++) {
+				String character = terminal.substring(index, index+1);
+				quotedString.addChild(new TerminalNode(character));
+			}
+			quotedString.addChild(dQuote);
+			element.addChild(charVal);
+
+			if (terminals.hasNext()) {
+				throw new IllegalStateException("only one terminal is currently support!");
+			}
+		} else if (actualRule.getClass().isAssignableFrom(Optional.class)) {
+			Optional rule = (Optional) actualRule;
+
+			Node option = new NonTerminalNode("option");
+			option.addChild(new TerminalNode("["));
+			option.addChild(generateAlternation(rule.getRule()));
+			option.addChild(new TerminalNode("["));
+			element.addChild(option);
+		} else {
+			Node option = new NonTerminalNode("group");
+			option.addChild(new TerminalNode("("));
+			option.addChild(generateAlternation(actualRule));
+			option.addChild(new TerminalNode(")"));
+			element.addChild(option);
+		}
+		return element;
 	}
 
 }
