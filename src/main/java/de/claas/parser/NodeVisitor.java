c6ddd335411c580c02c75fabdc2edb62c2274f7d
Claas Ahlrichs <claasahl@gmail.com>
2016-11-29 19:30:28 +0100

    reviewed documentation

--- src/main/java/de/claas/parser/NodeVisitor.java
+++ src/main/java/de/claas/parser/NodeVisitor.java
@@ -8,7 +8,7 @@ import de.claas.parser.results.TerminalNode;
 /**
  * Superclass of all node-based visitors. This class is intended to model a
  * visitor for {@link Node} instances and their children. Implementations of
- * this class will most likely process or interpret node-hierarchies.
+ * this class will process or interpret node-hierarchies.
  * <p>
  * This class resembles the <i>visitor</i> design pattern. It includes a
  * visit-method for all implementations of the {@link Node} class.
