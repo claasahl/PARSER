87771fa47a4ffc774e01c6dede058d028b136251
Claas Ahlrichs <claasahl@gmail.com>
2016-12-02 16:51:49 +0100

    reviewed documentation

--- src/main/java/de/claas/parser/exceptions/CyclicRuleException.java
+++ src/main/java/de/claas/parser/exceptions/CyclicRuleException.java
@@ -4,8 +4,9 @@ import de.claas.parser.Rule;
 
 /**
  * The exception {@link CyclicRuleException}. It is thrown to indicate that a
- * graph of {@link Rule}s contains a cyclic rule and that the throwing entity
- * cannot handle this cyclic rule.
+ * graph of {@link Rule}s contains a cyclic rule (i.e. a rule that directly or
+ * indirectly references itself) and that the throwing entity cannot handle this
+ * cyclic rule.
  * 
  * @author Claas Ahlrichs
  */
@@ -14,7 +15,8 @@ public class CyclicRuleException extends RuntimeException {
 	private static final long serialVersionUID = 4821496443992456798L;
 
 	/**
-	 * Constructs a new {@link CyclicRuleException} with default parameters.
+	 * Constructs a new {@link CyclicRuleException} with the specified
+	 * parameter.
 	 * 
 	 * @param rule
 	 *            the rule where the cycle was detected
