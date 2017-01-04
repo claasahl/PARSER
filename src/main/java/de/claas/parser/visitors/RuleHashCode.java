25d407ac53657f9f9b02caea2048dd1e3c2237c9
Claas Ahlrichs <claasahl@gmail.com>
2017-01-04 09:45:16 +0100

    reviewed documentation

--- src/main/java/de/claas/parser/visitors/RuleHashCode.java
+++ src/main/java/de/claas/parser/visitors/RuleHashCode.java
@@ -18,8 +18,9 @@ import de.claas.parser.rules.Repetition;
  * {@link RuleVisitor}. It is intended to determine a combined hash code for all
  * visited {@link Rule}s.
  * <p>
- * This visitor is meant for one-time use, only. As such, it should not be used
- * to determine multiple hash codes.
+ * This visitor is meant for one-time use, only. As such, every hash-code needs
+ * to be determined with a separate instance of this visitor. An instance of
+ * this visitor should not be used to determine multiple hash codes.
  *
  * @author Claas Ahlrichs
  */
